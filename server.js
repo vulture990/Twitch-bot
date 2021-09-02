5b4eb6648b0e120412d88d7a9bb507ab7c9e8203
unknown <mohamedsali602@live.com>
2021-09-02 12:28:06 +0200

    random strings

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 require('dotenv').config();
 const tmi = require('tmi.js');
 const { username } = require('tmi.js/lib/utils');
+const latestRandomString='';
+function makeRandomString(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
+var timer=60*1000*5;//5 minutes
+
+
+function botSpitsOutRandomString(length) {
+		latestRandomeString = makeRandomString(length);
+		client.say(channel,latestRandomString);
+}
 
 const client = new tmi.Client({
 	options: { debug: true },
@@ -19,10 +37,17 @@ client.connect();
 client.on('message', (channel, tags, message, self) => {
 	// Ignore echoed messages.
 	if(self) return;
+	botSpitsOutRandomString(5);
+	// setInterval(function botSpitsOutRandomString(length) {
+	// 	latestRandomeString = makeRandomString(length);
+	// 	client.say(channel,latestRandomString);
+	// },timer);
 	const isAdmin=tags.username ===process.env.TWITCH_BOT_USERNAME;
+	if(message=='!'+latestRandomString ){
+		client.say(channel, `Yo Congrats , @${tags.username}, you Got it,you Won!`);
+	}
 
 	if(message.toLowerCase() === '!entries') {
-		// "@alca, heya!"
 		client.say(channel, `@${tags.username}, You've Entred!`);
 		entires[tags.username]=username;
 		// store the users who enter the command for entries
@@ -34,3 +59,6 @@ client.on('message', (channel, tags, message, self) => {
 		// and this is just picking a random user from the array of users who enter the command
 	}
 });
+
+//the bot has to write a random code to the channel every n-minutes
+//the first viewer to type the random code wins the prize
